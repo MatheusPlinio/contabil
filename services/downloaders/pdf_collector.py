a2bb66a6aa7f90991cd3b086240aa2e4589219e6
MatheusPlinio <mplinio13@gmail.com>
2025-08-22 03:08:54 +0000

    feat: use .env for database configuration
    This commit introduces the use of a `.env` file for storing database configuration details.
    
    - The database credentials (user, password, host, port, and name) are now loaded from environment variables using the `python-dotenv` library.
    - The configuration is read from the `.env` file, which is then used to construct the `DATABASE_URL`.
    - This approach enhances security by preventing hardcoding sensitive information directly in the code, making it easier to manage configurations across different environments.

--- services/downloaders/pdf_collector.py
+++ services/downloaders/pdf_collector.py
@@ -11,13 +11,11 @@ class PDFCollector:
     def collect(self, year: int, month_name: str) -> list[str]:
         self.driver.get("https://www.natal.rn.gov.br/dom")
 
-        # Seleciona ano e mês
         Select(self.driver.find_element(By.NAME, "ano")
                ).select_by_visible_text(str(year))
         Select(self.driver.find_element(By.NAME, "mes")
                ).select_by_visible_text(month_name)
 
-        # Clica no botão para filtrar
         self.driver.find_element(
             By.CSS_SELECTOR, "button[data-attach-loading]").click()
 
@@ -28,7 +26,6 @@ class PDFCollector:
         pdf_urls = set()
 
         while True:
-            # Captura os links dos PDFs na página atual
             rows = self.driver.find_elements(
                 By.XPATH, "//table[@id='example']//tbody//tr")
             for row in rows:
@@ -38,14 +35,12 @@ class PDFCollector:
                     if href and href.lower().endswith(".pdf"):
                         pdf_urls.add(href)
 
-            # Paginação por números
             page_items = self.driver.find_elements(
                 By.XPATH, "//div[@id='example_paginate']//li")
             active_item = self.driver.find_element(
                 By.XPATH, "//div[@id='example_paginate']//li[contains(@class,'active')]")
             active_index = page_items.index(active_item)
 
-            # Se houver próxima página
             if active_index + 1 < len(page_items):
                 next_page_link = page_items[active_index +
                                             1].find_element(By.TAG_NAME, "a")
